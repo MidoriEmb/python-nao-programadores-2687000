fad2e823f608f8dffd335aaba79deece12a55232
JWAGATSU <julia.wagatsuma@embraer.com.br>
2026-04-14 11:25:06 -0300

    funcoes

--- exercicios/4-aprofundando-o-aprendizado/1-funcoes.py
+++ exercicios/4-aprofundando-o-aprendizado/1-funcoes.py
@@ -1,7 +1,18 @@
 # Crie uma função para selecionar o curso desejado em uma trilha profissional
-
 # Crie uma função para percorrer todos os níveis de um curso e imprimir na tela a informação do nível atual
-
 # Execute as funções
 
-        
+def selecionar_curso():
+    curso = input("Digite o numero do curso desejado (1-Python, 2-Java): ")
+    return curso
+
+def percorrer_niveis(nivel):
+    curso_escolhido = selecionar_curso()
+    curso_dic = {"1": "Python", "2": "Java"}
+    print(f"Você escolheu o curso de {curso_dic[curso_escolhido]}")
+    for i in range(1, nivel+1):
+        print(f"Você está no nível {i} do curso de {curso_dic[curso_escolhido]}.")
+    else:
+        print(f"Parabéns! Você concluiu o curso de {curso_dic[curso_escolhido]}.")
+
+percorrer_niveis(10)
